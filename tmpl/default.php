fe033d6b5c0cffa5b1d5c7aa0b1713410bb5c894
Maarten Blokdijk <maarten@cloudfaction.nl>
2019-08-13 14:48:08 +0200

    Update default.php

--- tmpl/default.php
+++ tmpl/default.php
@@ -5,28 +5,26 @@
  * @copyright   Copyright (C) 2019. All rights reserved.
  * @license     http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
  * @author      Maarten Blokdijk / www.joonextpro.com
- 
-*Below is set for instructional purposes only
-*Normaal
-*<iframe width="560" height="315" src="https://www.youtube.com/embed/vlDzYIIOYmM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
-*Privacy
-*<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/vlDzYIIOYmM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
-*Start seconde
-*<iframe width="560" height="315" src="https://www.youtube.com/embed/vlDzYIIOYmM?start=3" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
-*/
+ */
 //No Direct Access
 defined('_JEXEC') or die;
+
+if($jn_audio_only=='1'):
+  $jn_youtube_height="0 px";
+endif;
+
+if($jn_audio_only=='0'):
+
 ?>
 <style>
 .youtube-iframer {<?php echo $jn_youtube_css?>}
 </style>
 
+<?php endif;?>
+
 <div class="youtube-iframer">
 
-<iframe width="<?php echo $jn_youtube_width?>" height="<?php echo $jn_youtube_height?>" src="https://www.youtube<?php if ($jn_use_privacy=1){echo '-nocookie';}?>.com/embed/<?php echo $jn_youtube_file?>?start=<?php echo $jn_youtube_startsecond?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<iframe width="<?php echo $jn_youtube_width?>" height="<?php echo $jn_youtube_height?>" src="https://www.youtube.com/embed/<?php echo $jn_youtube_file?>?start=<?php echo $jn_youtube_startsecond?><?php if($jn_autoplay=='1'):?>&autoplay=1<?php endif;?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
 </div>
 
